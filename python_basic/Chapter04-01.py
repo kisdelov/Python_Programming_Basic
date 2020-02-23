a854c989d4a72a07f60848a64fbb50926d39bb37
JAE HUN <kidsevol@mju.ac.kr>
2020-02-23 20:22:05 +0900

    2월 23일

--- python_basic/Chapter04-01.py
+++ python_basic/Chapter04-01.py
@@ -58,5 +58,56 @@ if city2:
 else:
     print("Please enter your city")
 
+# 논리 연산자(중요)
+# and, or, not
+
+a = 75
+b = 40
+c = 10
+
+print('and :', a > b and b > c) # a > b > c
+print('or :', a > b or b > c)
+
+print('not :', not a > b)
+print('not :', not b > c)
+print(not True)
+print(not False)
+
+# 산술, 관계, 논리 우선순위
+# 산술 > 관꼐 > 논리
+print('e1 :', 3+12 > 7+3)
+print('e2 :', 5+10*3 > 7+3*20)
+print('e3 :', 5+10 > 3 and 7+3 == 10)
+print('e4 :', 5+10 > 0 and not 7+3 == 10)
+
+score1 = 90
+score2 = 'A'
+
+# 예4
+# 복수의 조건이 모두 참일 경우에 실행
+if score1 >= 90 and score2 == 'A':
+    print('Pass')
+else:
+    print('Fail')
+
+# 예5
+id1 = 'vip'
+id2 = 'admin'
+grade = 'platinum'
+
+if id1 == 'vip' or id2 == 'admin':
+    print('관리자 입장')
+
+if id2 == 'admin' and grade == 'platinum':
+    print('최상위 관리자')
+
+
+
+
+
+
+
+
+
 
 
