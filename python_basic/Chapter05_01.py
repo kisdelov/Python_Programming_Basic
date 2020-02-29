d3f6d25d1bc6ae1b8f6b23ada61a6323c87071e7
JAE HUN <kidsevol@mju.ac.kr>
2020-02-29 21:19:40 +0900

    2월 29일

--- python_basic/Chapter05_01.py
+++ python_basic/Chapter05_01.py
@@ -115,12 +115,16 @@ kwargs_func(name1='Lee', name2='Park')
 kwargs_func(name1='Lee', name2='Park', name3='Cho', sendSMS=True)
 
 # 전체 혼합
+
+
 def example(args_1, args_2, *args_3, **args_4):
     print(args_1, args_2, args_3, args_4)
+
 
 example(10, 20, 'Lee', 'Kim', 'Park', age1=20, age2=30, age3=40)
 
 # 중첩함수 : 함수 안에 함수 - 호출이 되지 않는다~
+
 
 def nested_func(num):
     def func_in_func(num):
@@ -128,10 +132,44 @@ def nested_func(num):
     print("In func")
     func_in_func(num+100)
 
+
 nested_func(100)
 
 # 실행불가
 # func_in_func() 부모 함수를 호출하지 않으면 정의되지 않는다
+
+# 람다식 예제
+# 메모리 절약, 가독성 향상, 코드 간결
+# 함수는 객체 생성 -> 리소스(메모리) 할당
+# 람다는 즉시 실행 함수(Heep 초기화) -> 메모리 초기화
+# 남발 시 가독성 오히려 감소
+
+'''
+def mul_func(x, y):
+    return x * y
+'''
+'''
+lambda x, y: x*y
+함수에 이름이 없다~
+'''
+
+# 일반적함수 -> 할당
+def mul_func(x, y):
+    return x*y
+
+print(mul_func(10, 50))
+mul_func_var = mul_func
+print(mul_func_var(20, 50))
+
+# 람다 함수 -> 할당
+lambda_mul_func = lambda x, y:  x * y
+print(lambda_mul_func(50, 50))
+
+def func_final(x, y, func):
+    print(x * y * func(100, 100))
+
+func_final(10, 20, lambda x, y : x * y)
+
 
 
 
