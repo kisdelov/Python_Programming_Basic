a396ae9fb29a61a7a69723bb18f50bf25a11f5e2
JAE HUN <kidsevol@mju.ac.kr>
2020-02-29 21:51:32 +0900

    2월 29일

---
+++ python_basic/Chapter05_02.py
@@ -0,0 +1,48 @@
+# Chapter05_02
+# 파이썬 사용자 입력
+# Input 사용법
+# 기본 타입(str)
+
+
+# # 예제 1
+# name = input("Enter Your Name : ")
+# grade = input("Enter Your Grade : ")
+# company = input("Enter Your Company name : ")
+
+# print(name, grade, company)
+
+
+# # 예제 2
+# number = input("Enter Number : ")
+# name = input("Enter Your Name : ")
+
+# print("type of number :", type(number), number * 3)
+# print("type of name :", type(name))
+
+# # 예제 3 (계산)
+# first_number =int(input("Enter number1 : "))
+# second_number =int(input("Enter number2 : "))
+# total =first_number + second_number
+# print("first + second :", total)
+
+# # 예제 4
+# float_number = float(input("Enter a float number : "))
+
+# print("input float :", float_number * 1.233.12)
+# print("input type :", type(float_number))
+
+# 예제 5
+print("FirstName - {0}, LastName - {1}".format(input("Enter first name : "), input("Enter Second name : ")))
+
+
+
+
+
+
+
+
+
+
+
+
+
