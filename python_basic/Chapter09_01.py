b6294c12ea683880a2ed08965643029564c04e81
JAE HUN <kidsevol@mju.ac.kr>
2020-03-12 22:53:29 +0900

    3월 12일

--- python_basic/Chapter09_01.py
+++ python_basic/Chapter09_01.py
@@ -23,8 +23,46 @@ print(cts)
 # 반드시 close
 f.close()
 
+# 예제 2
+with open('./resource/it_news.txt', 'r', encoding='UTF-8') as f:
+    c = f.read()
+    print(c)
+    print(iter(c))
+    print(list(c))
+print()
 
+# 예제 3
+# read() : 전체 읽기, read(10) : 10byte
+with open('./resource/it_news.txt', 'r', encoding='UTF-8') as f:
+    c = f.read(20)
+    print(c)
+    c = f.read(20)
+    print(c)
+    c = f.read(20)
+    print(c)
+    f.seek(0, 0)
+    c = f.read(20)
+    print(c)
 
+# 예제 4
+# readline : 한 줄 씩 읽기
+with open('./resource/it_news.txt', 'r', encoding='UTF-8') as f:
+    line = f.readline()
+    print(line)
+    line = f.readline()
+    print(line)
 
+print()
 
+# 예제 5
+# readlines : 전체를 읽은 후 라인 단위 리스트로 저장
+with open('./resource/it_news.txt', 'r', encoding='UTF-8') as f:
+    cts = f.readlines()
+    print(cts)
+    print()
+    for c in cts:
+        print(c, end='')
 
+print()
+
+# 파일 쓰기(write)
