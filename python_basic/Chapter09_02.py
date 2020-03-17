ef432d1b6bcc5c59bd0785520126fd57785cf2bb
JAE HUN <kidsevol@mju.ac.kr>
2020-03-17 22:43:53 +0900

    3월 17일

--- python_basic/Chapter09_02.py
+++ python_basic/Chapter09_02.py
@@ -47,11 +47,33 @@ with open('./python_basic/resource/test1.csv', 'r') as f:
             print(k,v)
         print('---------------')
 
+# 예제 4
+w = [[1,2,3],[4,5,6],[7,8,9],[10,11,12],[13,14,15],[16,17,18],[19,20,21]]
 
+with open('./python_basic/resource/write1.csv', 'w', encoding='utf-8') as f:
+    print(dir(csv))
+    wt = csv.writer(f)
 
+    # dir 확인
+    # print(dir(wt))
+    # 타입 확인
+    # print(type(wt))
 
+    for v in w:
+        wt.writerow(v)
 
+# 예제 5
+with open('./python_basic/resource/write2.csv', 'w', encoding='utf-8') as f:
+    # 필드명
+    fields = ['One', 'Two', 'Three']
+    
+    # Dicwriter
+    wt = csv.DictWriter(f, fieldnames=fields)
+    # Header writer
+    wt.writeheader()
 
-
+    for v in w:
+        wt.writerow({'One': v[0], 'Two': v[1], 'Three': v[2]})
+        
 
 
