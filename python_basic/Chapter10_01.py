bd9a9dff4137db589fcb4fd2c1596f1a25537b85
JAE HUN <kidsevol@mju.ac.kr>
2020-03-22 23:56:30 +0900

    3월 22일

--- python_basic/Chapter10_01.py
+++ python_basic/Chapter10_01.py
@@ -5,9 +5,9 @@
 import time
 
 # 처음 인사
-name = input("What is your name?")
+name = input("What is your name? ")
 
-print("Hi, ", + name, "Time to play hangman game!")
+print("Hi,",name, "Time to play hangman game!")
 
 print()
 
@@ -37,36 +37,36 @@ while turns > 0:
         # 정답 단어 내에 추측 문자가 포함되어 있는 경우
         if char in guesses:
             # 추측단어 출력
-            print(char, end='')
+            print(char, end=' ')
         else:
             # 틀린 경우 대시로 처리
-            print("_", end='')
+            print("_", end=' ')
             failed += 1
-        # 단어 추측이 성공한 경우
-        if failed == 0:
-            print()
-            print()
-            print('Congratulation! The Guesses is correct.')
-            # while 구문 중단
-            break
+    # 단어 추측이 성공한 경우
+    if failed == 0:
         print()
-
-        # 추측 단어 문자 단위 입력
         print()
-        guess = input("guess a character.")
+        print('Congratulation! The Guesses is correct.')
+        # while 구문 중단je
+        break
+    print()
 
-        # 단어 더하기
-        guesses += guess
-        
-        # 정답 단어에 추측한 문자가 포함되어 있지 않으면
-        if guess not in word:
-            # 기회 횟수 감소
-            turns -= 1
-            # 오류 메시지
-            print("Oops! Wrong")
-            # 남은 기회 출력
-            print("You have", turns, "more guesses!")
+    # 추측 단어 문자 단위 입력
+    print()
+    guess = input("guess a character. ")
 
-            if turns == 0:
-                # 실패 메시지
-                print("You hangman game failed. Bye!")
+    # 단어 더하기
+    guesses += guess
+    
+    # 정답 단어에 추측한 문자가 포함되어 있지 않으면
+    if guess not in word:
+        # 기회 횟수 감소
+        turns -= 1
+        # 오류 메시지
+        print("Oops! Wrong")
+        # 남은 기회 출력
+        print("You have", turns, "more guesses!")
+
+        if turns == 0:
+            # 실패 메시지
+            print("You hangman game failed. Bye!")
