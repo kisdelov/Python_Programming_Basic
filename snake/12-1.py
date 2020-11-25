8c3244e25accb945785033e5ee3d4480c94d5245
JAE HUN <kidsevol@mju.ac.kr>
2020-11-25 20:58:55 +0900

    뱀 게임 개선 1

--- snake/12-1.py
+++ snake/12-1.py
@@ -6,20 +6,15 @@
 # 사과가 놓인 뒤 일정한 시간이 지나면 사과의 색이 점점 흐려지다가 사과가 없어지도록 하자.
 # 뱀의 머리 블록에서 꼬리 블록으로 갈 수록 색이 점점 변하게 하자.
 
-
-import pygame              # ❶ 파이게임 모듈 임포트하기
+import pygame
 import time
 
 SCREEN_WIDTH = 400
 SCREEN_HEIGHT = 400
 BLOCK_SIZE = 20
 
-pygame.init()              # ❸ 파이게임을 사용하기 전에 초기화한다.
-
-# ❹ 지정한 크기의 게임 화면 창을 연다.
+pygame.init()
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-
-# time.sleep(3)              # ❺ 3초동안 기다린다
 
 RED = 255, 0, 0        # 적색:   적 255, 녹   0, 청   0
 GREEN = 0, 255, 0      # 녹색:   적   0, 녹 255, 청   0
@@ -28,22 +23,6 @@ PURPLE = 127, 0, 127   # 보라색: 적 127, 녹   0, 청 127
 BLACK = 0, 0, 0        # 검은색: 적   0, 녹   0, 청   0
 GRAY = 127, 127, 127   # 회색:   적 127, 녹 127, 청 127
 WHITE = 255, 255, 255  # 하얀색: 적 255, 녹 255, 청 255
-
-# # 화면 전체에 하얀 사각형 그리기
-# rect = pygame.Rect((0, 0), (SCREEN_WIDTH, SCREEN_HEIGHT))  # ❶
-# pygame.draw.rect(screen, WHITE, rect)  # ❷
-
-# # 화면 왼쪽 위에 녹색 정사각형 그리기
-# rect = pygame.Rect((0, 0), (40, 40))
-# pygame.draw.rect(screen, GREEN, rect)
-
-# # 화면 오른쪽 아래에 적색 직사각형 그리기
-# rect = pygame.Rect((340, 60), (60, 20))
-# pygame.draw.rect(screen, RED, rect)
-
-# pygame.display.update()  # ❸ 화면 새로고침
-
-# time.sleep(3)
 
 def draw_background(screen):
     """게임의 배경을 그린다."""
@@ -55,17 +34,6 @@ def draw_block(screen, color, position):
     block = pygame.Rect((position[1] * BLOCK_SIZE, position[0] * BLOCK_SIZE),
                         (BLOCK_SIZE, BLOCK_SIZE))
     pygame.draw.rect(screen, color, block)
-
-# draw_background(screen)
-# draw_block(screen, RED, (1, 1))
-# draw_block(screen, RED, (3, 1))
-# draw_block(screen, RED, (5, 1))
-# draw_block(screen, RED, (7, 1))
-# draw_block(screen, GREEN, (12, 10))
-# draw_block(screen, GREEN, (12, 11))
-# draw_block(screen, GREEN, (12, 12))
-# draw_block(screen, GREEN, (12, 13))
-# pygame.display.update()
 
 from datetime import datetime
 from datetime import timedelta
@@ -88,8 +56,8 @@ class Snake:
 
     def draw(self, screen):
         """뱀을 화면에 그린다."""
-        for position in self.positions:  # ❶ 뱀의 몸 블록들을 순회하며
-            draw_block(screen, self.color, position)  # 각 블록을 그린다
+        for position in self.positions:
+            draw_block(screen, self.color, position)
     
     def crawl(self):
         """뱀이 현재 방향으로 한 칸 기어간다."""
@@ -104,7 +72,7 @@ class Snake:
         elif self.direction == 'east':
             self.positions = [(y, x + 1)] + self.positions[:-1]
 
-    def turn(self, direction):  # ❶
+    def turn(self, direction):
         """뱀의 방향을 바꾼다."""
         self.direction = direction
 
@@ -127,52 +95,60 @@ class SnakeCollisionException(Exception):
 
 class Apple:
     """사과 클래스"""
-    color = RED  # 사과의 색
+    color = RED
 
     def __init__(self, position=(5, 5)):
-        self.position = position  # 사과의 위치
+        self.position = position
 
     def draw(self, screen):
         """사과를 화면에 그린다."""
-        draw_block(screen, self.color, self.position)  # ❷
+        draw_block(screen, self.color, self.position)
 
 class GameBoard:
     """게임판 클래스"""
-    width = 20   # 게임판의 너비
-    height = 20  # 게임판의 높이
-
+    width = 20
+    height = 20
+    x = range(0, width)
+    y = range(0, height)
+    # box = [(self.x[i], self.y[j]) for i in range(0, height) for j in range(0, width)]
+    box = []
+    
     def __init__(self):
-        self.snake = Snake()  # 게임판 위의 뱀
-        self.apple = Apple()  # 게임판 위의 사과
+        self.snake = Snake()
+        self.apple = Apple()
+        for i in self.y:
+            for j in self.x:
+                self.box.append((self.x[i], self.y[j]))
     
     def draw(self, screen):
         """화면에 게임판의 구성요소를 그린다."""
-        self.apple.draw(screen)  # ❸ 게임판 위의 사과를 그린다
-        self.snake.draw(screen)  # 게임판 위의 뱀을 그린다
+        self.apple.draw(screen)
+        self.snake.draw(screen)
     
     def put_new_apple(self):
         """게임판에 새 사과를 놓는다."""
-        self.apple = Apple((random.randint(0, 19), random.randint(0, 19)))  # ❷
-        for position in self.snake.positions:    # ❸ 뱀 블록을 순회하면서
-            if self.apple.position == position:  # 사과가 뱀 위치에 놓인 경우를 확인해
-                self.put_new_apple()             # 사과를 새로 놓는다
+        self.apple = Apple((random.randint(0, 19), random.randint(0, 19)))
+        for position in self.snake.positions:
+            if self.apple.position == position:
+                self.put_new_apple()
                 break
 
     def process_turn(self):
         """게임을 한 차례 진행한다."""
-        self.snake.crawl()  # 뱀이 한 칸 기어간다.
-        # ❶ 뱀의 머리와 사과가 닿았으면
+        self.snake.crawl()
+        
         if self.snake.positions[0] == self.apple.position:
-            self.snake.grow()     # 뱀을 한 칸 자라게 한다
-            self.put_new_apple()  # 사과를 새로 놓는다
-        # 뱀의 머리가 뱀의 몸과 부딛혔으면
+            self.snake.grow()
+            self.put_new_apple()
+
         if self.snake.positions[0] in self.snake.positions[1:]:
-            raise SnakeCollisionException()  # 뱀 충돌 예외를 일으킨다
+            raise SnakeCollisionException()
 
+        if self.snake.positions[0] not in self.box:
+            raise SnakeCollisionException()
 
-
-game_board = GameBoard()  # ❶ 게임판 인스턴스를 생성한다
-TURN_INTERVAL = timedelta(seconds=0.1)
+game_board = GameBoard()
+TURN_INTERVAL = timedelta(seconds=0.3)
 last_turn_time = datetime.now()
 
 while True:
@@ -180,11 +156,10 @@ while True:
     for event in events:
         if event.type == pygame.QUIT:
             exit()
-        if event.type == pygame.KEYDOWN:  # ❷ 화살표 키가 입력되면 뱀의 방향을 바꾼다
+        if event.type == pygame.KEYDOWN:
             if event.key in DIRECTION_ON_KEY:
                 game_board.snake.turn(DIRECTION_ON_KEY[event.key])
 
-    # ❷ 시간이 TURN_INTERVAL만큼 지날 때마다 게임을 한 차례씩 진행한다
     if TURN_INTERVAL < datetime.now() - last_turn_time:
         try:
             game_board.process_turn()
@@ -193,39 +168,5 @@ while True:
         last_turn_time = datetime.now()
 
     draw_background(screen)
-    game_board.draw(screen)  # ❷ 화면에 게임판을 그린다
+    game_board.draw(screen)
     pygame.display.update()
-
-# # ❶ 방향키 입력에 따라 바꿀 블록의 방향
-
-# block_direction = 'east'  # ❷ 블록의 방향
-# block_position = [0, 0]
-# last_moved_time = datetime.now()
-
-# while True:
-#     events = pygame.event.get()
-#     for event in events:
-#         if event.type == pygame.QUIT:
-#             exit()
-#         if event.type == pygame.KEYDOWN:
-#             # ❸ 입력된 키가 화살표 키면,
-#             if event.key in DIRECTION_ON_KEY:
-#                 # ❹ 블록의 방향을 화살표 키에 맞게 바꾼다 
-#                 block_direction = DIRECTION_ON_KEY[event.key]
-
-#     if timedelta(seconds=1) <= datetime.now() - last_moved_time:
-#         if block_direction == 'north':    # ❺ 1초가 지날 때마다
-#             block_position[0] -= 1        # 블록의 방향에 따라
-#         elif block_direction == 'south':  # 블록의 위치를 변경한다
-#             block_position[0] += 1
-#         elif block_direction == 'west':
-#             block_position[1] -= 1
-#         elif block_direction == 'east':
-#             block_position[1] += 1
-#         last_moved_time = datetime.now()
-
-#     draw_background(screen)
-#     draw_block(screen, GREEN, block_position)
-#     pygame.display.update()
-    # ❸ 화면을 계속 새로 그린다
-
